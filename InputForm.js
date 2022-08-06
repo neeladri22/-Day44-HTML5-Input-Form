c680fc2744318904cd48297e8c6d35c29b3344f4
neeladri22 <neeladri22@gmail.com>
2022-08-06 10:00:41 +0530

    intial code

---
+++ InputForm.js
@@ -0,0 +1,22 @@
+//UC1 - Validate First Name
+
+const text = document.querySelector('#text');
+const texterror = document.querySelector('.text-error');
+text.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(text.value))
+        texterror.textContent = "";
+    else texterror.textContent = "Name is Incorrect";
+});
+
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
+
+
+function submitMsg() {
+    alert("Form Submitted Successfully");
+}
